cdcfe408b1c577ee8d3029e78a3dfb2615315859
Swapnil1296 <swapnillandage79@gmail.com>
2022-06-09 00:08:45 +0530

    rendered items

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,30 +1,71 @@
-import React from "react";
-import styles from "./task.module.css";
-
+import React from 'react';
+import styles from './task.module.css';
+import Counter from '../Counter/Counter';
 const Task = ({taskItems, sendTasks, updateTask}) => {
   // console.log('task:', sendTasks);
   // NOTE: do not delete `data-testid` key value pair
   const handleChandge = () => {
- console.log("HI")
-     for (var i = 0; i < sendTasks.length; i++) {
-       if (sendTasks[i].id === sendTasks.id) {
-         let output = [...sendTasks];
-         output.splice(i, 1);
+    //  console.log("HI")
+    for (var i = 0; i < sendTasks.length; i++) {
+      if (sendTasks[i].id === sendTasks.id) {
+        let output = [...sendTasks];
+        output.splice(i, 1);
 
+        return updateTask(output);
+      }
+    }
+  };
+  const handleValueDone = () => {
+    let ans = [...sendTasks];
+    for (var i = 0; i < ans.length; i++) {
+      if (ans[i].id === taskItems.id) {
+        ans[i].done = !ans[i].done;
 
-         console.log('result', output);
-
-         return updateTask(output);
-       }
-     }
-  }
+        return updateTask(ans);
+      }
+    }
+  };
   return (
     <li data-testid="task" className={styles.task}>
       <input type="checkbox" data-testid="task-checkbox" />
       <div data-testid="task-text"></div>
       {/* Counter here */}
+        <input
+          onChange={() => handleValueDone()}
+          type="checkbox"
+          data-testid="task-checkbox"
+          checked
+        />
+      ) : (
+        <input
+          onChange={() => handleValueDone()}
+          type="checkbox"
+          data-testid="task-checkbox"
+        />
+      )}
+      {taskItems.done ? (
+        <div
+          className='container_one'
+          style={{
+            textDecoration: 'line-through',
+            textAlign: 'center',
+            width: '111px',
+          }}
+          data-testid="task-text"
+        >
+          {taskItems.text}
+        </div>
+      ) : (
+        <div
+          style={{textAlign: 'center', width: '80px'}}
+          data-testid="task-text"
+        >
+          {taskItems.text}
+        </div>
+      )}
+      <Counter />
       <button data-testid="task-remove-button" onClick={() => handleChandge()}>
-        +
+        🗑️
       </button>
     </li>
   );
